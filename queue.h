6b528eddea165098b5c4a4fd3c9dc32a45ff9843
Shraddha Datta <sdatta@hamilton.edu>
2022-04-30 16:21:56 -0400

    Until breadth-first initialization of tree

--- queue.h
+++ queue.h
@@ -1,5 +1,5 @@
-#ifndef TREE_H
-#define TREE_H
+#ifndef QUEUE_H
+#define QUEUE_H
 
 #include <vector> // for children
 #include <utility> // for coordinates
@@ -7,17 +7,20 @@
 class Queue
 {
 public:
-   Queue(); 
-   void enqueue(); 
-   void dequeue()
+   Queue();
+   void enqueue(const std::pair<int, int> item);
+   std::pair<int,int> dequeue();
+   bool empty() const;
 private:
    struct Node
    {
-      std::pair <int, int> _coordinates;
-      std::vector<Node> children;
-      Node* parent; 
-   }
-   Node* _root;
+      std::pair <int, int> coords;
+      Node* next;
+      Node* parent;
+   };
+   Node* _head;
+   Node* _tail;
+   size_t _size;
 };
 
 #endif
